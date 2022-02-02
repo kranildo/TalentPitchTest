8234a1f2a3429c045458100bc682c1a974ab2802
MaktubLTDA <95450345+MaktubLTDA@users.noreply.github.com>
2022-02-02 14:42:01 -0300

    feat: migration employees

---
+++ backend/database/migrations/2022_02_02_173444_table__employees.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class TableEmployees extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigIncrements('company_id');
+            $table->string('first_name', 255);
+            $table->string('last_name', 255);
+            $table->string('email', 255)->unique(); 
+            $table->number('phone'); 
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employees');
+    }
+}
