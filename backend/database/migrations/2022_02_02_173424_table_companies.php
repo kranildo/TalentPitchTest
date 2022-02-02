80fc35db91f066720269dfc87f9d47c0ad6c48d8
MaktubLTDA <95450345+MaktubLTDA@users.noreply.github.com>
2022-02-02 14:42:11 -0300

    feat: migration companies

---
+++ backend/database/migrations/2022_02_02_173424_table_companies.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class TableCompanies extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name', 255);
+            $table->string('email', 255)->unique();  
+            $table->string('logo ', 255);
+            $table->string('website ', 255);
+            $table->rememberToken();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
